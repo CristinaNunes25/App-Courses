d0aa066c5f6e5a156069c01aa4d74736d7f78625
Cristina Nunes <cristina.nunees@gmail.com>
2020-08-17 11:15:50 +0100

    V 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: '**', component
       }
     ])
 
